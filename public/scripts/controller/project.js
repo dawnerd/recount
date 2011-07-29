7eb68c8b2349c4ccfcd747cb0390f7a556c190d4
Troy Whiteley <troy.whiteley@demandmedia.com>
2011-07-29 14:59:14 -0700

    Added fancy animation.

--- public/scripts/controller/project.js
+++ public/scripts/controller/project.js
@@ -1,5 +1,34 @@
 var ProjectController = $.Fidel.extend({
   init: function() {
-    console.log('project init.')
+    var self = this;
+
+    this.el.find('.voteButton').bind('click', function(e){
+      self.showVoteBox(e);
+    });
+
+    this.el.find('.doneButton').bind('click', function(e){
+      self.hideVoteBox(e);
+    });
+
+  },
+  showVoteBox: function(e) {
+    if(e) e.preventDefault();
+
+    var parent = $(e.target).closest('.projectContainer'),
+        slider = parent.find('.slider');
+
+    console.log(parent, slider)
+
+    slider.animate({marginLeft: '-308px', duration: 250});
+  },
+  hideVoteBox: function(e) {
+    if(e) e.preventDefault();
+
+    var parent = $(e.target).closest('.projectContainer'),
+        slider = parent.find('.slider');
+
+    console.log(parent, slider)
+
+    slider.animate({marginLeft: '0', duration: 250});
   }
 });
