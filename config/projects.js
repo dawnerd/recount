292071e66a9d6b1ba642e0560b2ad4b1a528fbfa
Alex <alex.lea@demandmedia.com>
2011-07-29 11:07:03 -0700

    [AL] Css changes

--- config/projects.js
+++ config/projects.js
@@ -20,5 +20,27 @@ module.exports = [
       'Troy Whiteley',
       'Alex Lea'
     ]
+  },
+  {
+    id: 3,
+    name: 'Example project 2',
+    desc: 'This is a description for a project',
+    img: 'http://dummyimage.com/60x40/333/fff',
+    url: 'about:blank',
+    authors: [
+      'Troy Whiteley',
+      'Alex Lea'
+    ]
+  },
+  {
+    id: 4,
+    name: 'Example project 2',
+    desc: 'This is a description for a project',
+    img: 'http://dummyimage.com/60x40/333/fff',
+    url: 'about:blank',
+    authors: [
+      'Troy Whiteley',
+      'Alex Lea'
+    ]
   }
 ];
