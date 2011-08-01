00d8de45b65d6d2fb7ec0850f5c45ef3f5c20f4d
Troy Whiteley <troy.whiteley@demandmedia.com>
2011-08-01 16:04:09 -0700

    Added checks to ensure users cannot vote on two different projects at the same time.

--- public/scripts/controller/project.js
+++ public/scripts/controller/project.js
@@ -11,51 +11,102 @@ var ProjectController = $.Fidel.extend({
       self.submitVotes(e);
     });
 
+    this.el.find('.cancel').bind('click', function(e){
+      self.hideVoteBox(e);
+    });
+
     this.el.find('.checkbar').bind('click', function(e){
       self.toggleCheckbar(this, e);
     });
 
+    var storage = localStorage.getItem('vote-data') || '{}';
+    this.userData = JSON.parse(storage);
+
+    this.disablePolls();
   },
   showVoteBox: function(e) {
-    if(e) e.preventDefault();
+    if(e.preventDefault) e.preventDefault();
 
     var parent = $(e.target).closest('.projectContainer'),
-        slider = parent.find('.slider');
+        slider = parent.find('.slider'),
+        siblings = parent.siblings()
+        self = this;
+      
+    siblings.each(function(el){
+      var _e = {target:el};
+      self.hideVoteBox(_e);
+    });
 
     slider.animate({marginLeft: '-308px', duration: 250});
   },
   hideVoteBox: function(e) {
-    if(e) e.preventDefault();
+    if(e.preventDefault) e.preventDefault();
 
-    var parent = $(e.target).closest('.projectContainer'),
-        slider = parent.find('.slider');
+    var target = $(e.target);
+    if(target.hasClass('.projectContainer')) {
+      var parent = $(e.target).closest('.projectContainer');
+    } else {
+      var parent = target;
+    }
+
+    var slider = parent.find('.slider');
 
     slider.animate({marginLeft: '0', duration: 250});
   },
   submitVotes: function(e) {
     var parent = $(e.target).closest('.projectContainer'),
-        project_id = parent.find('.project').attr('data-project'),
+        project_id = parent.attr('data-project'),
         votes = parent.find('a[data-status="selected"]');
 
+    if(!this.userData.votes) this.userData.votes = [];
+
     for(var i = votes.length; i--;) {
+      var vote = votes[i],
+          poll_id = $(vote).attr('data-id');
+
+      this.userData.votes.push(poll_id+':'+project_id);
+
       socket.emit("vote", {
         project_id: project_id,
-        poll_id: $(votes[i]).attr('data-id')
+        poll_id: poll_id
       });
     }
+
+    localStorage.setItem('vote-data', JSON.stringify(this.userData));
+
+    this.disablePolls();
   },
   toggleCheckbar: function(element, e) {
     if(e) e.preventDefault();
 
     var vote = $(element),
-        status = vote.attr('data-status');
+        status = vote.attr('data-status'),
+        poll_id = vote.attr('data-id');
 
-    if(status === 'selected') {
-      vote.removeClass('selected');
-      vote.attr('data-status', '');
-    } else {
+    $('.checkbar[data-id="'+poll_id+'"]').attr('data-status', '').removeClass('selected');
+
+    if(!status || status === '') {
       vote.addClass('selected');
       vote.attr('data-status', 'selected');
+    }
+  },
+  disablePolls: function() {
+    if(this.userData.votes) {
+      var votes = this.userData.votes;
+      for(var i = votes.length; i--;) {
+        var vote = votes[i],
+            parts = vote.split(':'),
+            poll_id = parts[0],
+            project_id = parts[1];
+
+        $('.projectContainer .checkbar[data-id="'+poll_id+'"]')
+          .addClass('unavailable')
+          .attr('data-status', 'unavailable');
+        $('.projectContainer[data-project="'+project_id+'"] .checkbar[data-id="'+poll_id+'"]')
+          .removeClass('unavailable')
+          .addClass('voted')
+          .attr('data-status', 'voted');
+      }
     }
   }
 });
