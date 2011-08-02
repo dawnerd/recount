6dc421a3e4bdd18e4421eed37c0e4b9e0e28ddae
Alex <alex.lea@demandmedia.com>
2011-08-02 14:17:57 -0700

    [AL] Converted colors to function call

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,20 @@
 var project = new ProjectController({ el: $('#Projects') });
 
-if(colors!==undefined) {
-	// assume colors already exists
-	var target = $('#Results .poll');
-	var numP = colors.length;
-	var numC = colors[0]['colors'].length;
-	var p = 0;
-	target.each(function(){
-		var b = 0;
-		$('.bar', this).each(function() {
-			$(this).css({ 'background-color' : '#'+colors[p%numP]['colors'][b%numC] });
-			b++;
-		});
-		p++;  // next poll
-	});
+function colorPolls(target) {
+  target = $(target);
+  var numP = colors.length;
+  var numC = colors[0]['colors'].length;
+  var p = 0;
+  target.each(function(){
+    var b = 0;
+    $('.bar', this).each(function() {
+      $(this).css({ 'background-color' : '#'+colors[p%numP]['colors'][b%numC] });
+      b++;
+    });
+    p++;  // next poll
+  });
+}
+
+if(colors!==undefined && $('#Results').length>0) {
+  colorPolls('#Results .poll');
 }
