edbb5aa5a91f74b5b2674574c3a06475edb2e2ab
Alex <alex.lea@demandmedia.com>
2011-08-02 16:59:55 -0700

    [AL] Added new logo icon

--- config/projects.js
+++ config/projects.js
@@ -13,25 +13,23 @@ module.exports = [
   },
   {
     id: 2,
-    name: 'Example project 2',
+    name: 'Example project 2 has a really long name',
     desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vel leo nisi, a convallis enim. Maecenas condimentum magna ac velit eleifend vulputate. In hac habitasse platea dictumst. Maecenas rhoncus ultrices mi id vulputate. ',
     img: 'http://dummyimage.com/278x120/666/fff',
     url: 'about:blank',
     authors: [
-      'Troy Whiteley',
-      'Alex Lea'
+      'Einstein'
     ],
     count: 0
   },
   {
     id: 3,
-    name: 'Example project 3',
-    desc: 'This is a description for a project',
+    name: 'Woof Woof',
+    desc: 'Arf arf woof woof *meow*... wait I mean woof.',
     img: 'http://dummyimage.com/278x120/666/fff',
     url: 'about:blank',
     authors: [
-      'Troy Whiteley',
-      'Alex Lea'
+      'Snaggletooth the DEV-ouring dog'
     ],
     count: 0
   },
