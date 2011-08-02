beb696ebc5620281ec2e9abd8026a1e7d46bb24c
Alex <alex.lea@demandmedia.com>
2011-08-02 11:47:14 -0700

    [AL] Styling changes and addition of new color config file

---
+++ config/colors.js
@@ -0,0 +1,47 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Most creative',
+    description: 'This project shows extreme creativity'
+  },
+  {
+    id: 2,
+    name: 'Best overall',
+    description: 'Edges might be a little rough but this is the best overall'
+  },
+  {
+    id: 3,
+    name: 'Best Face',
+    description: 'Your face looks good'
+  },
+  {
+    id: 4,
+    name: 'Great Teeth',
+    description: 'Not really.'
+  },
+  {
+    id: 5,
+    name: '100% Beef',
+    description: 'Moooooooooooooooo'
+  },
+  {
+    id: 6,
+    name: 'Coolest',
+    description: 'AWWWWWW YEAHHH'
+  },
+  {
+    id: 7,
+    name: 'blahblahblah',
+    description: 'This project shows extreme creativity'
+  },
+  {
+    id: 8,
+    name: 'Most creative',
+    description: 'This project shows extreme creativity'
+  },
+  {
+    id: 9,
+    name: 'Most creative',
+    description: 'This project shows extreme creativity'
+  },
+];
